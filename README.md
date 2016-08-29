e9e4053b5ee9dec5c18627b546eff7a2dab5850a
Janel diBiccari <jdibiccari@thuzio.com>
2016-08-28 22:54:38 -0400

    Clean up unneccessary bits

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 
 ## Setup and Requirements
 1. Install node
-2. Install [mongodb](https://docs.mongodb.com/master/tutorial/install-mongodb-on-os-x/?_ga=1.117204739.446157408.1471830718) to use as our message data store
-	* [Here's](http://treehouse.github.io/installation-guides/mac/mongo-mac.html) a helpful resource if you run into any trouble
+2. Install requirements -- npm install
+3. Start the app -- npm run start
--- server.js
+++ server.js
@@ -4,73 +4,78 @@ const mongoose = require('mongoose')
 const app = express()
 const port = 3700
 const server = require('http').createServer(app)
-const routes = require('./app/routes')
+
 // Require our models
 const User = require('./app/models').User
 const Message = require('./app/models').Message
 
 // Connect to database
 mongoose.Promise = require('bluebird')
-mongoose.connect('mongodb://localhost/posthaste', (err) => {
-  if (err) throw err
+mongoose.connect('mongodb://posthaste:posthaste@ds017726.mlab.com:17726/posthaste', (err) => {
+	if (err) throw err
 })
 
 // Socket.io setup
 const io = require('socket.io')(server)
 
-// Static folder setup
+// App configuration
 app.use(express.static(path.join(__dirname,'/public')))
 
-// Set up router
-app.use('/', routes)
-
-
 server.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+	console.log(`Listening on port ${port}`)
 })
 
-const connections = []
 // Socket.io events
+const connections = []
+const messages = []
+
 io.on('connection', (socket) => {
-  socket.on('login', (username) => {
-    var promise = User.findOne({ username: username }).exec()
+	Message.find().sort('-created_at').limit(10).populate('user').then(messages => {
+		socket.emit('init', {
+			connections: connections,
+			messages: messages
+		})
+	})
 
-    promise.then(user => {
-      if (!user) {
-        const newUser = new User({ username: username })
-        return newUser.save()
-      }
-      return user
-    }).then(user => {
-      socket.user = user
-      connections.push(user)
-      const notification = `${user.username} has logged in`
-      io.sockets.emit('notify', notification)
-      io.sockets.emit('update count', connections.length)
-    }).catch(err => {
-      console.log(err)
-    })
-  })
+	socket.on('login', (username) => {
+		var promise = User.findOne({ username: username }).exec()
 
-  // New message arrives -- timestamp and push to clients
+		promise.then(user => {
+			if (!user) {
+				const newUser = new User({ username: username })
+				return newUser.save()
+			}
+			return user
+		}).then(user => {
+			socket.user = user
+			connections.push(user)
+			io.sockets.emit('notify', connections)
+			io.sockets.emit('update count', connections.length)
+		}).catch(err => {
+			console.log(err)
+		})
+	})
+
   socket.on('new msg', (msg) => {
-    console.log(socket.user)
-    const newMsg = new Message({ user: socket.user._id, msg: msg })
-    newMsg.save().then(msg => {
-      io.sockets.emit('new msg', msg, socket.user.username)
-    })
+	if (socket.user) {
+		const newMsg = new Message({ user: socket.user._id, msg: msg })
+		newMsg.save().then(msg => {
+			return Message.populate(msg, {path:'user'})
+		}).then(msg => {
+			io.sockets.emit('new msg', msg)
+		})
+	} else {
+		console.log('no user specified')
+	}
   })
 
   socket.on('disconnect', () => {
-    console.log(socket)
-    if (socket.user) {
-      notification = `${socket.user.username} has disconnected`
-      const index = connections.indexOf(socket.user)
-      console.log(index)
-      connections.splice(index, 1)
-      io.sockets.emit('notify', notification)
-      io.sockets.emit('update count', connections.length)
-    }
+	if (socket.user) {
+		const index = connections.indexOf(socket.user)
+		connections.splice(index, 1)
+		io.sockets.emit('notify', connections)
+		io.sockets.emit('update count', connections.length)
+	}
 
   })
 })
